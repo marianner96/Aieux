2c65482086efe35e3d4aba14ea79a0d8b446c10b
Marianne Rentchler <rentchlerm@eisti.eu>
2016-11-25 11:41:44 +0100

    On essaye de faire bien la bdd = on a un fichier forms.py et models.py

--- Formulaires/models.py
+++ Formulaires/models.py
@@ -1,35 +1,17 @@
 #!/usr/bin/python
 # -*- coding:Utf-8 -*-
-
-from __future__ import unicode_literals
+import datetime
 
 from django.db import models
+from django.utils import timezone
 
-from django import forms
-
-from django.forms import ModelForm
-
-GENRES = (
-    ('feminin', 'Féminin'),
-    ('masculin', 'Masculin'),
-)
-
-## Définition des formulaires
-class InscripConnecForm(forms.Form):
-    inscrip_nom = forms.CharField(label='Nom ', required='required')
-    inscrip_prenom = forms.CharField(label='Prenom ', required='required')
-    inscrip_genre = forms.ChoiceField(label='Genre', widget=forms.RadioSelect, choices=GENRES, required='required')
-    inscrip_ddn = forms.CharField(label='Date de naissance ', required='required')
-    inscrip_email = forms.EmailField(label='E-mail', required='required')
-    inscrip_mdp = forms.CharField(widget=forms.PasswordInput(), label="Mot de passe", required='required')
-
-    connec_email = forms.EmailField(label='E-mail', required='required')
-    connec_mdp = forms.CharField(widget=forms.PasswordInput(), label="Mot de passe", required='required')
+class Famille(models.Model):
+    nom = models.CharField(max_length=30)
+    nb_personnes = models.IntegerField(max_length=50)
+    
 
 
-
-
-class modifForm(forms.Form):
+"""class modifForm(forms.Form):
     #image = 
     modif_nom = forms.CharField(label='Nom ', required='required', initial="nom bdd")
     modif_prenom = forms.CharField(label='Prenom ', required='required', initial="prénom bdd")
@@ -42,3 +24,4 @@ class modifForm(forms.Form):
     modif_description = forms.CharField(widget=forms.Textarea(), label='Description', initial="description bdd")
     modif_mdp = forms.CharField(widget=forms.PasswordInput(), label="Mot de passe", required='required')
 
+"""
