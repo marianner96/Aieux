1b59b39d9c2cbfb58a0d0a3b4db649ca178912e6
Lea Thibout <lea.thibout@eisti.fr>
2016-11-25 13:34:35 +0100

    commit des urls.py

--- Formulaires/urls.py
+++ Formulaires/urls.py
@@ -1,13 +1,19 @@
 from django.conf.urls import url
+from django.contrib import admin
 
-from . import views
+from Formulaires import views
+
 
 app_name = 'Formulaires'
 urlpatterns = [
     url(r'^$', views.accueilForm, name='accueilForm'),
-    url(r'^inscriptionForm/$', views.inscriptionForm, name='inscriptionForm'),
-    url(r'^connectionForm/$', views.connectionForm, name='connectionForm'),
     url(r'^modificationForm/$', views.modificationForm, name='modificationForm'),
     url(r'^Felicitations/$', views.Felicitations, name='Felicitations'),
+    url(r'^Menubis/$', views.Menubis, name='Menubis'),
+    url(r'^Menu/$', views.Menu, name='Menu'),
+    url(r'^Form_famille/$', views.Form_famille, name='Form_famille'),
+    url(r'^Form_famille_ajoutmembre/$', views.Form_famille_ajoutmembre, name='Form_famille_ajoutmembre'),
+    url(r'^Form_event/$', views.Form_event, name='Form_event'),
+    url(r'^Confirm_ajoutevent/$', views.Confirm_ajoutevent, name='Confirm_ajoutevent'),
 ]
 
