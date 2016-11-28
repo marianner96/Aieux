472caac9387f9025bad6da22e814240d46d818ec
Marianne Rentchler <rentchlerm@eisti.eu>
2016-11-28 14:28:08 +0100

    Résolution du merge sur models.py

--- Formulaires/models.py
+++ Formulaires/models.py
@@ -5,24 +5,18 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-<<<<<<< HEAD
+
 GENRES = (
     ('feminin', 'Feminin'),
     ('masculin', 'Masculin'),
 )
-=======
-import datetime
 
-from django import forms
-from django.utils import timezone
-from django.forms import ModelForm
->>>>>>> 6b30769b78af72b0597741177f4155b0f21a77b1
 
 class Famille(models.Model):
     nom = models.CharField(max_length=30)
     nb_personnes = models.IntegerField()
 
-<<<<<<< HEAD
+
 class Utilisateur(models.Model):
     nom = models.CharField(max_length=30)
     prenom = models.CharField(max_length=30)
@@ -37,13 +31,9 @@ class Utilisateur(models.Model):
     rang = models.IntegerField(default=0)
     moderateur = models.IntegerField(default=1)
     
+    
 class Arbre(models.Model):
     famille = models.ForeignKey(Famille, on_delete=models.CASCADE)
-=======
-GENRES = (
-    ('feminin', 'Féminin'),
-    ('masculin', 'Masculin'),
-)
 
 class ClassFormInscription(forms.Form):
     nom = forms.CharField(label='Nom ', required='required')
@@ -63,8 +53,8 @@ class ClassFormConnection(forms.Form):
 class ClassmodifForm(forms.Form):
     #image = 
     nom = forms.CharField(label='Nom ', required='required', initial="nom bdd")
-    prenom = forms.CharField(label='Prenom ', required='required', initial="prénom bdd")
-    prenoms_autre = forms.CharField(label='Autres prénoms', initial="autres prénom bdd")
+    prenom = forms.CharField(label='Prenom ', required='required', initial="prenom bdd")
+    prenoms_autre = forms.CharField(label='Autres prenoms', initial="autres prenom bdd")
     genre = forms.ChoiceField(label='Genre', widget=forms.RadioSelect, choices=GENRES, required='required')
     ddn = forms.CharField(label='Date de naissance ', required='required', initial="date de naissance bdd")
     email = forms.EmailField(label='E-mail', required='required', initial="email bdd")
@@ -72,19 +62,3 @@ class ClassmodifForm(forms.Form):
     profession = forms.CharField(label='Profession', initial="profession bdd")
     description = forms.CharField(widget=forms.Textarea(), label='Description', initial="description bdd")
     mdp = forms.CharField(widget=forms.PasswordInput(), label="Mot de passe", required='required')
->>>>>>> 6b30769b78af72b0597741177f4155b0f21a77b1
-
-"""class modifForm(forms.Form):
-    #image = 
-    modif_nom = forms.CharField(label='Nom ', required='required', initial="nom bdd")
-    modif_prenom = forms.CharField(label='Prenom ', required='required', initial="prénom bdd")
-    modif_prenoms_autre = forms.CharField(label='Autres prénoms', initial="autres prénom bdd")
-    modif_genre = forms.ChoiceField(label='Genre', widget=forms.RadioSelect, choices=GENRES, required='required')
-    modif_ddn = forms.CharField(label='Date de naissance ', required='required', initial="date de naissance bdd")
-    modif_email = forms.EmailField(label='E-mail', required='required', initial="email bdd")
-    modif_postal = forms.CharField(label='Adresse postale', initial="adresse postale bdd")
-    modif_profession = forms.CharField(label='Profession', initial="profession bdd")
-    modif_description = forms.CharField(widget=forms.Textarea(), label='Description', initial="description bdd")
-    modif_mdp = forms.CharField(widget=forms.PasswordInput(), label="Mot de passe", required='required')
-
-"""
