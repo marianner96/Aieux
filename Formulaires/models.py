36d58a507d78530c6e7c76d139fe0d9a003494cf
Lea Thibout <lea.thibout@eisti.fr>
2016-12-01 21:11:39 +0100

    Modif urls.py models.py views.py accueilForm.html InscriptionForm.html

--- Formulaires/models.py
+++ Formulaires/models.py
@@ -5,22 +5,94 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-import datetime
-
 from django import forms
-from django.utils import timezone
 from django.forms import ModelForm
 
-class Famille(models.Model):
-    nom = models.CharField(max_length=30)
-    nb_personnes = models.IntegerField(max_length=50)
-    
 
 GENRES = (
     ('feminin', 'Féminin'),
     ('masculin', 'Masculin'),
 )
 
+# id : identifiant unique de la famille (généré automatiquement)
+# nom : nom de la famille
+# nb_personnes : nombres de personnes présentes dans la famille
+class Famille(models.Model):
+    nom = models.CharField(max_length=30)
+    nb_personnes = models.IntegerField()
+
+    def __str__(self):
+        return self.nom
+
+
+# id : identifiant unique de l'utilisateur (généré automatiquement)
+# nom : nom de l'utilisateur
+# prenom : prénom de l'utilisateur
+# genre : genre de l'utilisateur
+# ddn : date de naissance de l'utilisateur
+# email : e-mail de l'utilisateur
+# mdp : mot de passe chiffré
+# adresse : adresse de l'utilisateur
+# profession : profession de l'utilisateur
+# nationalite : nationalite de l'utilisateur
+# description : description de l'utilisateur (optionnel)
+# id_famille : identifiant de la famille si il y appartient
+# rang : droit de l'utilisateur : 0 si c'est un utilisateur classique, 1 si c'est un historien et 2 si c'est un administrateur
+# moderateur : savoir si l'utilisateur est modérateur de son groupe famille. Par défaut il l'est donc c'est égal à 1, sinon c'est égal à 0
+class Utilisateur(models.Model):
+    nom = models.CharField(max_length=30)
+    prenom = models.CharField(max_length=30)
+    genre = models.CharField(max_length=8, choices=GENRES)
+    ddn = models.DateField(blank=True, null=True)
+    email = models.EmailField()
+    mdp = models.CharField(max_length=10)
+    adresse = models.CharField(max_length=200)
+    profession = models.CharField(max_length=60)
+    nationalite = models.CharField(max_length=60)
+    description = models.CharField(max_length=200)
+    famille = models.ForeignKey(Famille, on_delete=models.CASCADE, default=0)
+    rang = models.IntegerField(default=0)
+    moderateur = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.email
+
+   
+# id : identifiant unique de l'arbre de la famille généré automatiquement)
+# id_famille : identifiant de la famille auquel appartient l'arbre  
+class Arbre(models.Model):
+    famille = models.ForeignKey(Famille, on_delete=models.CASCADE)
+
+
+# id_arbre : l'identifiant de l'arbre sur lequel ce fait est relié
+# code : code du fait : 1 si naissance, 2 si mariage et 3 si décès
+# date_fait : date du fait historique
+# nom : nom de la personne concernée par ce fait
+# prenom : prenom de la personne concernée par ce fait
+#autre_nom : nom du conjoint (de la conjointe) si mariage
+#autre_prenom : prenom du conjoint (de la conjointe) si mariage
+class Fait_historique(models.Model):
+    arbre = models.ForeignKey(Arbre, on_delete=models.CASCADE)
+    code = models.IntegerField()
+    date_fait = models.DateField()
+    nom = models.CharField(max_length=30)
+    prenom = models.CharField(max_length=30)
+    autre_nom = models.CharField(max_length=30, null=True)
+    autre_prenom = models.CharField(max_length=30, null=True)
+
+
+###### Léa #####
+class UtilisateurForm(forms.Form):
+    nom = forms.CharField(label='Nom ', required='required')
+    prenom = forms.CharField(label='Prenom ', required='required')
+    genre = forms.ChoiceField(label='Genre', widget=forms.RadioSelect, choices=GENRES, required='required')
+    ddn = forms.CharField(label='Date de naissance ', required='required')
+    email = forms.EmailField(label='E-mail', required='required')
+    mdp = forms.CharField(widget=forms.PasswordInput(), label="Mot de passe", required='required')
+
+
+
+"""
 class ClassFormInscription(forms.Form):
     nom = forms.CharField(label='Nom ', required='required')
     prenom = forms.CharField(label='Prenom ', required='required')
@@ -49,7 +121,7 @@ class ClassmodifForm(forms.Form):
     description = forms.CharField(widget=forms.Textarea(), label='Description', initial="description bdd")
     mdp = forms.CharField(widget=forms.PasswordInput(), label="Mot de passe", required='required')
 
-"""class modifForm(forms.Form):
+class modifForm(forms.Form):
     #image = 
     modif_nom = forms.CharField(label='Nom ', required='required', initial="nom bdd")
     modif_prenom = forms.CharField(label='Prenom ', required='required', initial="prénom bdd")
@@ -61,5 +133,4 @@ class ClassmodifForm(forms.Form):
     modif_profession = forms.CharField(label='Profession', initial="profession bdd")
     modif_description = forms.CharField(widget=forms.Textarea(), label='Description', initial="description bdd")
     modif_mdp = forms.CharField(widget=forms.PasswordInput(), label="Mot de passe", required='required')
-
 """
--- Formulaires/views.py
+++ Formulaires/views.py
@@ -1,109 +1,110 @@
 #!/usr/bin/python
 # -*- coding:Utf-8 -*-
 
-from django.http import HttpResponse
-from django.shortcuts import render
-from bottle import Bottle, template, request, run
-from django.http import HttpResponseRedirect
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, render_to_response
 from django.template import Context, loader, RequestContext
-from django.shortcuts import render_to_response
-
 from django.views import generic
+from getpass import getpass
 
-from .models import ClassFormInscription, ClassFormConnection, ClassmodifForm
+from .models import Utilisateur, Famille, Arbre, Fait_historique, UtilisateurForm
 
-def accueilForm(request):
-	return render(request, 'accueilForm.html')
-
-
-############Tests inscription connection#############
+#def accueilForm(request):
+#	return render(request, 'accueilForm.html')
 
 
-##########################
-
-
-
-def accueilForm(request):
+def InscriptionForm(request):
 	if request.method == 'POST':
 		#On s'occupe du formulaire d'inscription
-		FormInscription = ClassFormInscription(request.POST)
+		FormInscription = UtilisateurForm(request.POST)
 #		FormConnection = ClassFormConnection(request.POST)
 
 		if FormInscription.is_(valid):
 			#Il faut enregistrer tout ça dans la BDD ...
 			#En fait je crois que c'est form.save() et c'est tout mais bon ..
-			nom = FormInscription.cleaned_data['nom']
+			
+			"""nom = FormInscription.cleaned_data['nom']
 			prenom = FormInscription.cleaned_data['prenom']
 			genre = FormInscription.cleaned_data['genre']
 			ddn = FormInscription.cleaned_data['ddn']
 			email = FormInscription.cleaned_data['email']
-			mdp = FormInscription.cleaned_data['mdp']
+			mdp = FormInscription.cleaned_data['mdp']"""
 
-			FormInscription.save()
-
-			
-#		if FormConnection.is_(valid):
-		#On s'ocucpe du formulaire de connection
-			#Il faut enregistrer tout ça dans la BDD ...
-			#En fait je crois que c'est form.save() et c'est tout mais bon ..
-#			email = FormConnection.cleaned_data['email']
-#			mdp = FormConnection.cleaned_data['mdp']
-
-#			FormConnection.save()
-
-			return render_to_response('accueilForm.html', {'FormInscription':FormInscription},  
-				contect_instance=RequestContext(request))
-	else: 
-		FormInscription = ClassFormInscription()
-	return render(request, 'accueilForm.html', {'FormInscription':FormInscription},
-		)
-
-
-def FormConnection(request):
-	if request.method == 'POST':
-		#On s'occupe du formulaire d'inscription
-		form = ClassFormConnection(request.POST)
-		if form.is_(valid):
-
-			mail = form.cleaned_data['email']
-			mdp = form.cleaned_data['mdp']
+			form = Utilisateur(
+				nom = FormInscription.cleaned_data['nom'], 
+				prenom = FormInscription.cleaned_data['prenom'],
+				genre = FormInscription.cleaned_data['genre'],
+				ddn = FormInscription.cleaned_data['ddn'],
+				email = FormInscription.cleaned_data['email'],
+				mdp = hashlib.sha1(FormInscription.cleaned_data['mdp']).hexdigest())
 
 			form.save()
 
-			return render_to_response('templates/accueilForm.html', {'FormConnection':form},  
-				contect_instance=RequestContext(request))
+			# il faudrait vérifier si une famille existe avec ce nom
+			# si oui --> changer rang
+			# si non --> la créer
+
+			#ajouter id de la famille à familler_id du gars
+			# form.famille_id = familledugars.id
+			
+			return render_to_response('InscriptionForm.html', {'FormInscription':FormInscription},  
+				context_instance=RequestContext(request))
 	else: 
-		form = ClassFormConnection()
-	return render(request, 'accueilForm.html', {'FormConnection':form})
+		FormInscription = UtilisateurForm()
+	return render(request, 'InscriptionForm.html', {'FormInscription':FormInscription},
+		)
+
+
+# pour décrypté un mot de passe, il faut crypté celui qu'on vient de recevoir avec celui deja dans la bdd
+def accueilForm(request):
+	if request.method == 'POST':
+		#On s'occupe du formulaire de connection
+		FormConnection = UtilisateurForm(request.POST)
+		if FormConnection.is_(valid):
+
+			mail = FormConnection.cleaned_data['email']
+			mdp = FormConnection.cleaned_data['mdp']
+
+			user = Utilisateur.objects.get(email=mail)
+			
+			#if (hashlib.sha1(mdp).hexdigest() == user.mdp):
+				#on se connecte
+			#else:
+				#on envoie un message d'erreur
+			
+			return render_to_response('accueilForm.html', {'FormConnection':FormConnection},  
+				context_instance=RequestContext(request))
+	else: 
+		FormConnection = UtilisateurForm()
+	return render(request, 'accueilForm.html', {'FormConnection':FormConnection})
 
 
 
 def modificationForm(request):
 	if request.method == 'POST':
-		form = modifForm(request.POST)
-		if form.is_(valid):
+		FormModif = modifForm(request.POST)
+		if FormModif.is_(valid):
 			#Il faut enregistrer tout ça dans la BDD ...
-			#En fait je crois que c'est form.save() et c'est tout mais bon ..
+			#En fait je crois que c'est FormModif.save() et c'est tout mais bon ..
 			#image = 
-			nom = form.cleaned_data['nom']
-			prenom = form.cleaned_data['prenom']
-			prenoms_autre = form.cleaned_data['prenoms_autre']
-			genre = form.cleaned_data['genre']
-			ddn = form.cleaned_data['ddn']
-			email = form.cleaned_data['email']
-			postal = form.cleaned_data['postal']
-			profession = form.cleaned_data['profession']
-			description = form.cleaned_data['description']
-			mdp = form.cleaned_data['mdp']
+			nom = FormModif.cleaned_data['nom']
+			prenom = FormModif.cleaned_data['prenom']
+			prenoms_autre = FormModif.cleaned_data['prenoms_autre']
+			genre = FormModif.cleaned_data['genre']
+			ddn = FormModif.cleaned_data['ddn']
+			email = FormModif.cleaned_data['email']
+			postal = FormModif.cleaned_data['postal']
+			profession = FormModif.cleaned_data['profession']
+			description = FormModif.cleaned_data['description']
+			mdp = FormModif.cleaned_data['mdp']
 
-			form.save()
+			FormModif.save()
 
-			return render_to_response('modificationForm.html', {'form':form}, 
-				contect_instance=RequestContext(request))
+			return render_to_response('modificationForm.html', {'FormModif':FormModif}, 
+				context_instance=RequestContext(request))
 	else: 
-		form = modifForm()
-	return render(request, 'modificationForm.html', {'form':form})
+		FormModif = modifForm()
+	return render(request, 'modificationForm.html', {'FormModif':FormModif})
 
 
 def Felicitations(request):
