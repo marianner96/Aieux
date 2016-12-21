d53e35f43587a135a977010491738f2b56559594
Lea Thibout <lea.thibout@eisti.fr>
2016-12-21 14:21:25 +0100

    modif?

--- Formulaires/forms.py
+++ Formulaires/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
 class RejoindreForm(forms.Form):
-    famille_id = forms.IntegerField(label='Famille')
+    famille_id = forms.IntegerField(label='Famille', required=False)
+    ajout = forms.CharField(label='ajout')
